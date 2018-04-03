630c6883b32785356c311fd6f3c3820dad9f7cae
RatajczakMelanie <ratajczakmelanie@gmail.com>
2018-04-03 16:18:50 +0200

    Aufgabe 1.2 fertiggestellt

---
+++ Aufgabe_1-2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Aufgabe_1_2
+{
+    class Program
+    {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        static bool[] usedsubject = {false, false, false, false, false, false};
+        static bool[] usedverb = {false, false, false, false, false, false};
+        static bool[] usedobject = {false, false, false, false, false, false}; 
+        
+        static void Main(string[] args)
+        {
+            for(int i=0; i<subjects.Length; i++){
+                string a = GetVerse();
+                Console.WriteLine(a);
+            }            
+        }  
+
+        public static string GetVerse(){
+
+            string vers;
+            Random rnd = new Random();
+            int cntSubj;
+            int cntVerb = rnd.Next(subjects.Length);
+            int cntObj = rnd.Next(subjects.Length);
+
+            do{
+                cntSubj = rnd.Next(subjects.Length);
+            } while (usedsubject[cntSubj] == true);
+            do{
+                cntVerb = rnd.Next(verbs.Length);
+            } while (usedverb[cntVerb] == true);
+            do{
+                cntObj = rnd.Next(objects.Length);
+            } while (usedobject[cntObj] == true);
+
+            vers = subjects[cntSubj] + " " + verbs[cntVerb] + " " + objects[cntObj];
+
+            usedsubject[cntSubj] = true;
+            usedverb[cntVerb] = true;
+            usedobject[cntObj] = true;
+
+            return vers;
+        }
+    }    
+}
