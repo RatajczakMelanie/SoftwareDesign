4791ff8fd61477da2ccbdc5de460233219ff9f1e
RatajczakMelanie <ratajczakmelanie@gmail.com>
2018-04-18 18:51:15 +0200

    Aufgabe 3

---
+++ Aufgabe3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aufgabe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           toHexal();
+        }
+
+        static int toHexal(int dec){
+            int factor;
+            int [] residuals = new int[] {};
+            if(dec <= 1023 && dec >= 0){
+                do{
+                    int modulo = dec % 6;
+                    int dec_minus_modulo = dec - modulo;
+                    factor = dec_minus_modulo/6;
+
+                }while(factor != 0);
+            }
+        
+            //Math.Exp(System.Double)
+            
+        }
+    }
+}
