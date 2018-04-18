ebf5cf63d724f651469b04e1d61f7c6e1ac47df9
RatajczakMelanie <ratajczakmelanie@gmail.com>
2018-04-18 20:34:04 +0200

    toHexal

--- Aufgabe3/Program.cs
+++ Aufgabe3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aufgabe3
 {
@@ -6,21 +8,32 @@ namespace Aufgabe3
     {
         static void Main(string[] args)
         {
-           toHexal();
+           Console.WriteLine(toHexal(231));
         }
 
-        static int toHexal(int dec){
+        public static int toHexal(int dec){
             int factor;
-            int [] residuals = new int[] {};
+            int hexal = 0;
+            System.Collections.Generic.List<int> residuals = new System.Collections.Generic.List<int>();
             if(dec <= 1023 && dec >= 0){
                 do{
                     int modulo = dec % 6;
                     int dec_minus_modulo = dec - modulo;
+                    residuals.Add(modulo);
+                    //residuals.Insert(0, dec_minus_modulo);
                     factor = dec_minus_modulo/6;
-
+                    dec = factor;
+                    
+                    //string a = residuals.ToString();
+                    //Console.WriteLine(a);
                 }while(factor != 0);
             }
-        
+            residuals.Reverse();
+            for(int i = 0; i<=residuals.Count-1; i++){
+                hexal += residuals[i] * Convert.ToInt32(Math.Pow(10,residuals.Count-i-1));
+            }
+             
+            return hexal;
             //Math.Exp(System.Double)
             
         }
