4eaf1f2bb159f378bbc7fbc1ecc541811d66fbf9
RatajczakMelanie <ratajczakmelanie@gmail.com>
2018-04-25 13:28:03 +0200

    Aufgabe 4 angefangen

---
+++ Aufgabe 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aufgabe_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        }
+
+        public class Tree{
+            public string Root;
+            public string Child;
+            public string Parent;
+        }
+
+        public void CreateNode() {
+            
+        }
+
+        Tree tree = new Tree();
+
+        
+    }
+}
