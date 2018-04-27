a04a02e0e68bd538bf38a5151d407217c97ed054
RatajczakMelanie <ratajczakmelanie@gmail.com>
2018-04-27 19:58:42 +0200

    Fortschritt

--- Aufgabe 4/Program.cs
+++ Aufgabe 4/Program.cs
@@ -23,16 +23,15 @@ namespace Aufgabe_4
             child1.Append_Child(grand13);
             var grand21 = tree.CreateNode("grand21");
             child2.Append_Child(grand21);
-            // child1.RemoveChild(grand12);
+             child1.Remove_Child(grand12);
 
-            //root.PrintTree(); 
+            Console.WriteLine(root.PrintTree()); 
         }
         
         public class TreeNode<T>{
             private T _item;
             private TreeNode<T> _parentNode;
-            private TreeNode<T>[] _children;
-            private int _n = 0;
+            private List <TreeNode<T>> _children;
             
             public TreeNode()
             {
@@ -46,40 +45,31 @@ namespace Aufgabe_4
                 _item = item;
                 _parentNode.Append_Child(this);
             }
-             public TreeNode<T> CreateNode(T item)
+              public TreeNode<T> CreateNode(T item)
                 {
-                    TreeNode<T> node = new TreeNode<T>(item);
+                    TreeNode<T> node = new TreeNode<T>();
+                    node._item = item;
                     return node;
                 }
-            public class Tree <G>
-            {
-               
-            }
+            
              
 
             public void Append_Child(TreeNode<T> child)
-            {   
-                // If necessary, grow the array
-                if (_n == _children.Length)
-                {
-                    TreeNode<T>[] oldArray = _children;
-                    _children = new TreeNode<T>[oldArray.Length+1];
-                    Array.Copy(oldArray, _children, _n);
-                    
+            {   if (_children.Count != 0)
+                {_children.Add(child);
+                child._parentNode = this;}else{
+                    Console.WriteLine("Nö");
                 }
-                _children[_n] = child;
-                child._parentNode = this;
-                _n++;
-
-
-
+                
                 
             }
 
-            
+            public void Remove_Child(TreeNode<T> child){
+                    _children.Remove(child);
+            }
 
-            public void Remove_Child(TreeNode<T> child, TreeNode<T> parentNode){
-                    
+            public String PrintTree(){
+                return _item.ToString();
             }
         }   
 
