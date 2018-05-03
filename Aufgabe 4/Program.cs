c8e1b8405e02cddef77191930482f2159589fe86
RatajczakMelanie <ratajczakmelanie@gmail.com>
2018-05-03 15:22:42 +0200

    find

--- Aufgabe 4/Program.cs
+++ Aufgabe 4/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Aufgabe_4
 {
@@ -8,6 +10,7 @@ namespace Aufgabe_4
     {
         static void Main(string[] args)
         {
+
             var tree = new TreeNode<String>();
             var root = tree.CreateNode("root");
             var child1 = tree.CreateNode("child1");
@@ -24,6 +27,12 @@ namespace Aufgabe_4
             child2.Append_Child(grand21);
              child1.Remove_Child(grand12);
             root.PrintTree(""); 
+            
+             List<TreeNode<String>> resultList = root.Find_Node("grand11", new List<TreeNode<String>>());
+
+             if(resultList.Count > 0){
+                 Console.WriteLine("Das gesuchte Wort wurde " + resultList.Count() + " Mal gefunden.");
+             }
         }
         
         public class TreeNode<T>{
@@ -70,9 +79,18 @@ namespace Aufgabe_4
                     _children.Remove(child);
             }
 
-            public string Find_Node(TreeNode<T> item){
-                List<TreeNode<T>> _foundNodes =_children.FindAll(item);
-                return "Hallo";
+            public List<TreeNode<T>> Find_Node(T search, List<TreeNode<T>> listToReturn){
+                _item = person;
+                if (_item.Equals(search)) {
+                    listToReturn.Add(this);
+                }
+                if(_children != null){
+                    foreach(var child in _children)
+                    {
+                        child.Find_Node(search, listToReturn);
+                    }
+                }
+                return listToReturn;
             }
 
             public void PrintTree(String Sternchen){
